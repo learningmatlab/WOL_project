287238c06c08e98c49f12a922654ebbbe263a40d
itheima <2814240743@qq.com>
2026-06-06 22:25:25 +0800

    init: Vue3 uni-app 空白项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+
+onLaunch(() => {
+  console.log('App Launch')
+})
+
+onShow(() => {
+  console.log('App Show')
+})
+
+onHide(() => {
+  console.log('App Hide')
+})
+</script>
+
+<style>
+page {
+  background-color: #f8f8f8;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <view class="container">
+    <view class="title">
+      <text class="title-text">WOL Project</text>
+    </view>
+    <view class="content">
+      <text class="desc">Vue3 + uni-app 空白项目已就绪</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+// 首页逻辑
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.title {
+  margin-bottom: 40rpx;
+}
+
+.title-text {
+  font-size: 48rpx;
+  font-weight: bold;
+  color: #333333;
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.desc {
+  font-size: 28rpx;
+  color: #999999;
+}
+</style>
